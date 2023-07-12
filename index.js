2247f09f86ebdc0be3e544c9eb20a0cfb586e090
Richard Koranteng <richard@rkkoranteng.com>
2023-07-12 18:46:38 -0400

    feat(git-action): dynamic blog add

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+String.prototype.splice = function (idx, rem, str) {
+    return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+};
+
+const getArticles = async () => {
+    const result = await fetch('https://rkkoranteng.com/')
+        .then(response => response.json())
+
+    return result.articles.map(({ title, slug }) => ({ title, slug }));
+}
+
+const generateReadMe = async () => {
+    const readMeData = {
+        articles: (await getArticles()).slice(0, 5)
+    };
+
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), readMeData);
+        fs.writeFileSync('README.md', output);
+    });
+}
+
+generateReadMe();
